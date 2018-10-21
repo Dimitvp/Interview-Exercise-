c7e7c779b1b24ddc1a3accb100c92e0d98543cc1
Dimitar Nikolov <zeitgeistdimi@gmail.com>
2018-10-21 12:41:14 +0300

    First exercise - Palindrome words

---
+++ Exercises/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Exercises
+{
+    class Program
+    {
+        public static bool IsPalindrome(string word)
+        {
+            var lowerString = word.ToLower();
+            for (int fL = 0, lL = lowerString.Length - 1; fL < lL; fL++, lL--){
+                if (lowerString[fL] != lowerString[lL])
+                {
+                    return false;
+                }
+            }
+            return true;
+
+        }
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(IsPalindrome("Deleveled"));
+        }
+    }
+}
